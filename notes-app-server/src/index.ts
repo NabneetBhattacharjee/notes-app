f480f4f93755b04d77502ea4faf2c982559ce878
NabneetBhattacharjee <nabneet.bhattacharjee@my.jcu.edu.au>
2024-09-24 00:50:32 +1000

    Created client and backend

---
+++ notes-app-server/src/index.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/notes", async (req, res) => {
+  const notes = await prisma.note.findMany();
+  res.json(notes);
+});
+
+app.post("/api/notes", async (req, res) => {
+  const { title, content } = req.body;
+  if (!title || !content) {
+    return res.status(400).json({ message: "Title and content are required" });
+  }
+  try {
+    const note = await prisma.note.create({
+      data: { title, content },
+    });
+    res.json(note);
+  } catch (error) {
+    res.status(500).send("Something went wrong");
+  }
+});
+
+app.get("/api/notes", async (req, res) => {
+  res.json({ message: "success" });
+});
+
+app.listen(5000, () => {
+  console.log("server running on localhost:5000");
+});
